b71eea259e04120805e7ab1916dfb4476d6ba570
kostyakoskos <50551774+kostyakoskos@users.noreply.github.com>
2020-04-04 15:22:15 +0300

    Shifr Simple Replace

--- Program.cs
+++ Program.cs
@@ -1,142 +1,86 @@
-﻿//Программа шифрования/дешифрования символов кирилицы шифром Цезаря
-//Подключение библиотек
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
 using System.IO;
 
-namespace ShifrCezar
+namespace ShifrSimpleReplace
 {
     class Program
     {
         static void Main()
         {
-            //Цикл для повтора задачи
+            char[,] keys = new char[66, 2];// 66 for all russian alphabet
+
+            using (TextReader reader = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\key.txt", Encoding.Default)) //путь и имя файла
+            {
+                string str;
+                char[] myStr = new char[2];// for every input srtring for 2 char
+                int count = 0;
+                while ((str = reader.ReadLine()) != null) //пока читается
+                {
+                    myStr = str.ToCharArray();// every string to 2 char
+                    keys[count, 0] = str[0];
+                    keys[count, 1] = str[1];
+                    count++;
+                }
+            }
             do
             {
-                string path = @"C:\Education\Term 4\KM\ShifrCezar\ShifrCezar\in.txt";
-                //Переменная выбора шифрования/дешифрования
                 uint k = 0;
-                //Строка, к которой применяется шифрованияе/дешифрование
                 string s = "";
-                //Строка - результат шифрования/дешифрования
                 string result = "";
-                //Величина сдвига при шифровании/дешифровании
-                uint shift;
-                //Вывод сообщения на экран
                 Console.WriteLine("1 to crypt, 2- decrypt");
-                //Считывание переменной выбора, пока она не станет равной 1 или 2
                 while ((k != 1) && (k != 2))
                 {
-                    //Считывание переменной k, если введенные данные имеют тип uint
                     uint.TryParse(Console.ReadLine(), out k);
-                    //Вывод сообщения об ошибке, если k != 1 или k != 2
                     if ((k != 1) && (k != 2))
                         Console.WriteLine("Input mistake");
                 }
-                //Вывод сообщения на экран
-                Console.WriteLine("input shift amount");
-                //Считывние величины сдвига
-                while (!uint.TryParse(Console.ReadLine(), out shift))
-                {
-                    //Если введена неверная величина сдвига (отрицательное число, или не число)
-                    Console.WriteLine("Input mistake");
-                }
-                //Если величина сдвига больше длины алфавита кирилицы
-                if (shift > 32)
-                    shift = shift % 32;
-                //Если выбрано шифрование
+                //crypt
                 if (k == 1)
                 {
-                    //Вывод сообщения на экран
+                    string path = @"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\decrypt.txt";
                     Console.WriteLine("String reading drom file!");
-                    //Считывание строки
                     s = File.ReadAllText(path, Encoding.Default);
-                    //Выполение шифрования
-                    //Цикл по каждому символу строки
+                    //crypt
+                    //cycle for every char of string
                     for (int i = 0; i < s.Length; i++)
                     {
-                        //Если не кириллица
-                        if (((int)(s[i]) < 1040) || ((int)(s[i]) > 1103))
-                            result += s[i];
-                        //Если буква является строчной
-                        if ((Convert.ToInt16(s[i]) >= 1072) && (Convert.ToInt16(s[i]) <= 1103))
+                        for (int z = 0; z < 66; z++)
                         {
-                            //Если буква, после сдвига выходит за пределы алфавита
-                            if (Convert.ToInt16(s[i]) + shift > 1103)
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) + shift - 32);
-                            //Если буква может быть сдвинута в пределах алфавита
-                            else
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) + shift);
-                        }
-                        //Если буква является прописной
-                        if ((Convert.ToInt16(s[i]) >= 1040) && (Convert.ToInt16(s[i]) <= 1071))
-                        {
-                            //Если буква, после сдвига выходит за пределы алфавита
-                            if (Convert.ToInt16(s[i]) + shift > 1071)
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) + shift - 32);
-                            //Если буква может быть сдвинута в пределах алфавита
-                            else
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) + shift);
+                            if (String.Equals(s[i], keys[z, 0]))
+                            {
+                                result += keys[z, 1];
+                                break;
+                            }
                         }
                     }
                     //Вывод на экран зашифрованной строки
                     Console.WriteLine("String successfully crypt!");
-                    StreamWriter sr = new StreamWriter(@"C:\Education\Term 4\KM\ShifrCezar\ShifrCezar\cryprt.txt", false);
+                    StreamWriter sr = new StreamWriter(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrCezar\\crypt.txt", false);
                     sr.Write(result);
                     sr.Close();
                     Console.WriteLine(result);
                 }
-                //Если было выбрано дешифрование
+                //decrypt
                 if (k == 2)
                 {
-                    //Вывод сообщения на экран
                     Console.WriteLine("String reading from file!");
-                    //Считывание строки
-                    //s = File.ReadAllText(path, Encoding.Default);- не читает если есть Encoding.Default
-                    StreamReader sr = new StreamReader(@"C:\Education\Term 4\KM\ShifrCezar\ShifrCezar\in.txt");
+                    StreamReader sr = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\in.txt");
                     s = sr.ReadToEnd();
                     sr.Close();
-                    //Time.Start();- вообще не нужно!
-                    //Выполение дешифрования
-                    //Цикл по каждому символу строки
                     for (int i = 0; i < s.Length; i++)
                     {
-                        if (Convert.ToInt16(s[i]) == 32)
-                            result += ' ';
-                        //Если буква является строчной
-                        if ((Convert.ToInt16(s[i]) >= 1072) && (Convert.ToInt16(s[i]) <= 1103))
+                        for (int z = 0; z < 66; z++)
                         {
-                            //Если буква, после сдвига выходит за пределы алфавита
-                            if (Convert.ToInt16(s[i]) - shift < 1072)
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) - shift + 32);
-                            //Если буква может быть сдвинута в пределах алфавита
-                            else
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) - shift);
-                        }
-                        //Если буква является прописной
-                        if ((Convert.ToInt16(s[i]) >= 1040) && (Convert.ToInt16(s[i]) <= 1071))
-                        {
-                            //Если буква, после сдвига выходит за пределы алфавита
-                            if (Convert.ToInt16(s[i]) - shift < 1040)
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) - shift + 32);
-                            //Если буква может быть сдвинута в пределах алфавита
-                            else
-                                //Добавление в строку результатов символ
-                                result += Convert.ToChar(Convert.ToInt16(s[i]) - shift);
+                            if (String.Equals(s[i], keys[z, 1]))
+                            {
+                                result += keys[z, 0];
+                                break;
+                            }
+
                         }
                     }
-                    //Вывод на экран дешифрованной строки
-                    Console.WriteLine("String successfulle decrypt!");
-                    StreamWriter sr1 = new StreamWriter(@"C:\Education\Term 4\KM\ShifrCezar\ShifrCezar\decrypt.txt", false);
+                    StreamWriter sr1 = new StreamWriter(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\decrypt.txt", false);
                     sr1.Write(result);
                     sr1.Close();
                     Console.WriteLine(result);
