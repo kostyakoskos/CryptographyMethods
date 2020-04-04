c9c872458d35c300d827f847f02207ddc2d9ca3d
kostyakoskos <50551774+kostyakoskos@users.noreply.github.com>
2020-04-04 15:24:00 +0300

    Shifr Simple Replace

--- Program.cs
+++ Program.cs
@@ -1,122 +1,92 @@
 ﻿using System;
-using System.IO;
 using System.Text;
+using System.IO;
 
-namespace ShifrVizenera
+namespace ShifrSimpleReplace
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //making vigener table
-            int lenStr = 32, lenColumn = 32, countI = 1072, myCounter = 0;
-            string key = "", input = "", result = "", decrypt = "", resDecrypt = "";
-            char[,] letters = new char[lenStr, lenColumn];
-            for (int i = 0; i < lenStr; i++)
-            {
-                for (int j = 0; j < lenColumn; j++)
-                {
-                    if (countI + j <= 1103)
-                    {
-                        letters[i, j] = Convert.ToChar(countI + j);
-                    }
-                    else
-                    {
-                        letters[i, j] = Convert.ToChar(countI + j - 32);
-                    }
-                }
-                countI++;
-            }
+            char[,] keys = new char[66, 2];// 66 for all russian alphabet
 
-            using (TextReader reader = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrVizenera\\key.txt", Encoding.Default))
+            using (TextReader reader = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\key.txt", Encoding.Default)) //путь и имя файла
             {
                 string str;
-                while ((str = reader.ReadLine()) != null)
+                char[] myStr = new char[2];// for every input srtring for 2 char
+                int count = 0;
+                while ((str = reader.ReadLine()) != null) //пока читается
                 {
-                    key = str;
+                    myStr = str.ToCharArray();// every string to 2 char
+                    keys[count, 0] = str[0];
+                    keys[count, 1] = str[1];
+                    count++;
                 }
             }
-
-            //decrypt
-            using (TextReader reader = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrVizenera\\in.txt", Encoding.Default))
+            do
             {
-                string str;
-                while ((str = reader.ReadLine()) != null)
+                uint k = 0;
+                string s = "";
+                string result = "";
+                Console.WriteLine("1 to crypt, 2- decrypt");
+                while ((k != 1) && (k != 2))
                 {
-                    input = str;
+                    uint.TryParse(Console.ReadLine(), out k);
+                    if ((k != 1) && (k != 2))
+                        Console.WriteLine("Input mistake");
                 }
-            }
-
-            char[] arr = new char[input.Length];
-            arr = input.ToCharArray();
-            char[] arrKey = new char[key.Length];
-            arrKey = key.ToCharArray();
-            for (int l = 1; l <= arr.Length; l++)
-            {
-                for (int i = 0; i < lenStr; i++)
+                //crypt
+                if (k == 1)
                 {
-                    if (String.Equals(letters[i, 0], arrKey[myCounter % l]))// find keyword letter
+                    string path = @"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\decrypt.txt";
+                    Console.WriteLine("String reading drom file!");
+                    s = File.ReadAllText(path, Encoding.Default);
+                    //crypt
+                    //cycle for every char of string
+                    for (int i = 0; i < s.Length; i++)
                     {
-                        for (int j = 0; j < lenColumn; j++)// cycle in column
+                        for (int z = 0; z < 66; z++)
                         {
-                            if (String.Equals(letters[i, j], arr[l - 1]))
+                            if (String.Equals(s[i], keys[z, 0]))
                             {
-                                result += letters[0, j - 1];
-                                if (myCounter == arrKey.Length - 1)
-                                    myCounter = 0;
-                                else myCounter++;
-                                i = lenStr;
-                                j = lenColumn;
+                                result += keys[z, 1];
+                                break;
                             }
                         }
                     }
+                    //Вывод на экран зашифрованной строки
+                    Console.WriteLine("String successfully crypt!");
+                    StreamWriter sr = new StreamWriter(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrCezar\\crypt.txt", false);
+                    sr.Write(result);
+                    sr.Close();
+                    Console.WriteLine(result);
                 }
-            }
-            Console.WriteLine("String successfully decrypt!");
-            StreamWriter sr = new StreamWriter(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrVizenera\\decrypt.txt", false);
-            sr.Write(result);
-            sr.Close();
-
-            //crypt
-            using (TextReader reader = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrVizenera\\decrypt.txt", Encoding.Default))
-            {
-                string str;
-                while ((str = reader.ReadLine()) != null)
+                //decrypt
+                if (k == 2)
                 {
-                    decrypt = str;
-                }
-            }
-
-            char[] crypt = new char[decrypt.Length];
-            crypt = decrypt.ToCharArray();
-
-            myCounter = 0;
-
-            for (int l = 1; l <= crypt.Length; l++)// cyclr for every letter of decrypt string
-            {
-                for (int i = 0; i < lenStr; i++)// cecle for find key letter
-                {
-                    if (String.Equals(letters[i, 0], arrKey[myCounter % l]))
+                    Console.WriteLine("String reading from file!");
+                    StreamReader sr = new StreamReader(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\in.txt");
+                    s = sr.ReadToEnd();
+                    sr.Close();
+                    for (int i = 0; i < s.Length; i++)
                     {
-                        for (int j = 0; j < lenColumn; j++)// cycle for find descrypt word letter
+                        for (int z = 0; z < 66; z++)
                         {
-                            if(String.Equals(letters[j,0], crypt[l - 1]))
+                            if (String.Equals(s[i], keys[z, 1]))
                             {
-                                resDecrypt += letters[i, j + 1];
-                                if (myCounter == arrKey.Length - 1)
-                                    myCounter = 0;
-                                else myCounter++;
-                                i = lenColumn;
-                                j = lenStr;
+                                result += keys[z, 0];
+                                break;
                             }
+
                         }
                     }
+                    StreamWriter sr1 = new StreamWriter(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrSimpleReplace\\decrypt.txt", false);
+                    sr1.Write(result);
+                    sr1.Close();
+                    Console.WriteLine(result);
                 }
-            }
-            Console.WriteLine("String successfully crypt!");
-            StreamWriter sr1 = new StreamWriter(@"C:\\Education\\Term 4\\KM\\ShifrCezar\\ShifrVizenera\\crypt.txt", false);
-            sr1.Write(resDecrypt);
-            sr1.Close();
+                Console.WriteLine("To Exit input Escape");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
